8cc28eab1d3ba2370b071392c9e5a50a1eeda72d
HP <solomonmach2004@gmail.com>
2025-03-15 22:41:17 +0300

    initial project files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/cards", (req, res) => {
     const cards = [
         { id: "card1", title: "Python Encryptor", description: "A simple fun python script to encrypt text files.", image: "/images/encryptor.jpg",github_link: "https://github.com/solomon-mash/Python-Encryptor/", web_link: "https://github.com/solomon-mash/Python-Encryptor/", email_link: "https://www.google.com"},
         { id: "card2", title: "MediaFlow 🎵🎥", description: "A sleek and intuitive video and music player built with Flutter.", image: "/images/media.png",github_link: "https://github.com/solomon-mash/Flutter-Media-Player/", web_link: "https://github.com/solomon-mash/Flutter-Media-Player/", email_link: "https://www.google.com" }, 
-        { id: "card3", title: "Portfolio Website", description: "A personal portfolio website that showcases my skills and projects done.", image: "/images/portfolio0_KQJR5MY.jpg", github_link: "https://github.com/solomon-mash", web_link: "http://solomon-macharia.vercel.app", email_link: "https://www.google.com" },
+        { id: "card3", title: "Portfolio Website", description: "A personal portfolio website that showcases my skills and projects done.", image: "/images/portfolio.png", github_link: "https://github.com/solomon-mash", web_link: "http://solomon-macharia.vercel.app", email_link: "https://www.google.com" },
        
     ];
     res.json(cards);
